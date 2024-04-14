9238e7d67b22ead8db5bd06f9100ed7f705b817b
arkadiusz-czerwinski <arek.pawel.czerwinski@gail.com>
2024-04-14 21:44:00 +0200

    test

--- job.py
+++ job.py
@@ -1,8 +1,14 @@
 import wandb
-
+import torch
 settings = wandb.Settings(disable_git=True)
 
 with wandb.init(settings=settings) as run:
     run.log({"hello": "world"})
     for i in range(10):
         run.log({"metric": i})
+    run.log(
+        {
+            "accelerator": "gpu" if torch.cuda.is_available() else "cpu",
+
+        }
+    )
