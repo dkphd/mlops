a9e61d3ccd89ec6c650cee0581472ed375164ea9
arkadiusz-czerwinski <arek.pawel.czerwinski@gail.com>
2024-04-14 22:09:25 +0200

    torch tests

--- job.py
+++ job.py
@@ -7,3 +7,13 @@ with wandb.init(settings=settings) as run:
     for i in range(10):
         run.log({"metric": i})
     
+    for i in range(10):
+        if torch.cuda.is_available():
+            run.log({"cuda": 1})
+        else:
+            run.log({"cuda": 0})
+
+
+
+
+    
