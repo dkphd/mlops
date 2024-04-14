9147e3edea4260d4622fe43d227542f630270007
arkadiusz-czerwinski <arek.pawel.czerwinski@gail.com>
2024-04-14 21:57:35 +0200

    test

--- job.py
+++ job.py
@@ -6,9 +6,4 @@ with wandb.init(settings=settings) as run:
     run.log({"hello": "world"})
     for i in range(10):
         run.log({"metric": i})
-    run.log(
-        {
-            "accelerator": "gpu" if torch.cuda.is_available() else "cpu",
-
-        }
-    )
+    
