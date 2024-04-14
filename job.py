88e9f4a62e2735d280567e5c90c3524c7af853e9
Damian Kucharski <c.DamianKucharski@gmail.com>
2024-04-14 19:05:22 +0200

    merge

--- job.py
+++ job.py
@@ -1,5 +1,14 @@
 import torch
 import torch.nn as nn
+import wandb
+
+config = {"epochs": 100, "w":2, "b":3}
+
+entity = "phd-dk"
+project = "mlops"
+job_name = "test_job"
+
+settings = wandb.Settings(job_name=job_name)
 
 class LinearModel(nn.Module):
     def __init__(self):
@@ -10,24 +19,31 @@ class LinearModel(nn.Module):
         return self.linear(x)
     
 
-x = torch.linspace(-1, 1, 100).view(-1, 1)
-y = 2 * x + 3
+with wandb.init(
+    entity=entity, config=config, project=project, settings=settings
+) as run:
+    config = wandb.config
 
-model = LinearModel()
-criterion = nn.MSELoss()
-optimizer = torch.optim.SGD(model.parameters(), lr=0.01)
+    x = torch.linspace(-1, 1, 100).view(-1, 1)
+    y = config.w * x + config.b
 
-for epoch in range(1000):
-    inputs = x
-    target = y
+    model = LinearModel()
+    criterion = nn.MSELoss()
+    optimizer = torch.optim.SGD(model.parameters(), lr=0.01)
 
-    optimizer.zero_grad()
-    outputs = model(inputs)
-    loss = criterion(outputs, target)
-    loss.backward()
-    optimizer.step()
-    print('epoch {}, loss {}'.format(epoch, loss.item()))
+    for epoch in range(1, config.epochs):
+        inputs = x
+        target = y
 
-print('w = {}, b = {}'.format(model.linear.weight.item(), model.linear.bias.item()))
-print(f"Should be w = 2, b = 3")
-torch.save(model.state_dict(), 'model.pth')
+        optimizer.zero_grad()
+        outputs = model(inputs)
+        loss = criterion(outputs, target)
+        loss.backward()
+        optimizer.step()
+        print('epoch {}, loss {}'.format(epoch, loss.item()))
+        wandb.log({"loss": loss, "epoch": epoch})
+
+    print('w = {}, b = {}'.format(model.linear.weight.item(), model.linear.bias.item()))
+    print(f"Should be w = {config.w}, b = {config.b}")
+
+    wandb.run.log_code()
