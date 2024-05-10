1784e6b6d6f1d0cc46db4bb4b2a6ece27f2e9e1a
Arkadiusz Czerwiński <42278982+arkadiusz-czerwinski@users.noreply.github.com>
2024-05-10 17:22:37 +0200

    Update job.py

--- job.py
+++ job.py
@@ -1,17 +1,23 @@
 import wandb
 import torch
-settings = wandb.Settings(disable_git=True)
+run = wandb.init(
+    project="physionet_article",
+    # name=f"resnet18_{'_'.join(new_config.folds_train)}_{train_config.activation}",
+    name="test",
+    entity="phd-dk",
+)
+        
 
-with wandb.init(settings=settings) as run:
-    run.log({"hello": "world"})
-    for i in range(10):
-        run.log({"metric": i})
-    
-    for i in range(10):
-        if torch.cuda.is_available():
-            run.log({"cuda": 1})
-        else:
-            run.log({"cuda": 0})
+
+run.log({"hello": "world"})
+for i in range(10):
+    run.log({"metric": i})
+
+for i in range(10):
+    if torch.cuda.is_available():
+        run.log({"cuda": 1})
+    else:
+        run.log({"cuda": 0})
 
 
 
